8cb6e10f30851817fe446c6125bcaa470bfdde39
Your Name <you@example.com>
2024-06-09 19:37:52 +0600

    add: code to work with db

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type application struct {
@@ -13,21 +16,45 @@ type application struct {
 }
 
 func main() {
-	addr := flag.String("addr", ":4000", "HTTP network address")
+	addr := flag.String("addr", ":4000", "HTTP сетевой адрес")
+	dsn := flag.String("dsn", "snippetbox.db", "Путь к SQLite3 базе данных")
 	flag.Parse()
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// Открытие базы данных SQLite3
+	db, err := sql.Open("sqlite3", *dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
+
+	// Создание экземпляра application
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
 	}
+
+	// Создание сервера HTTP
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		// Call the new app.routes() method to get the servemux containing our routes.
-		Handler: app.routes(),
+		Handler:  app.routes(), // Использование маршрутов из приложения
 	}
-	infoLog.Printf("Starting server on %s", *addr)
-	err := srv.ListenAndServe()
+
+	// Запуск сервера
+	infoLog.Printf("Start server on %s", *addr)
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module snippetbox.aza
 
 go 1.20
+
+require github.com/mattn/go-sqlite3 v1.14.22 // indirect
