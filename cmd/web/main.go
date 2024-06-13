2d78c2404fee6b5be5847a38a40038d1d12ae179
azamat21355 <sdk1nv2@gmail.com>
2024-06-13 19:37:19 +0600

    change comments (99)

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -22,32 +22,36 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	// Открытие базы данных SQLite3
+	// open sqlite 3 db
+	// To keep the main() function tidy I've put the code for creating a connection
+	// pool into the separate openDB() function below. We pass openDB() the DSN
+	// from the command-line flag
 	db, err := sql.Open("sqlite3", *dsn)
 	if err != nil {
 		errorLog.Fatal(err)
 	}
 	defer db.Close()
 
-	// Создание экземпляра application
+	// create app for loggin
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
 	}
 
-	// Создание сервера HTTP
+	// create server
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  app.routes(), // Использование маршрутов из приложения
+		Handler:  app.routes(),
 	}
 
-	// Запуск сервера
+	// start server
 	infoLog.Printf("Start server on %s", *addr)
 	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
 
+// The openDB() function wraps sql.Open() and returns a sql.DB connection poo
 func openDB(dsn string) (*sql.DB, error) {
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
